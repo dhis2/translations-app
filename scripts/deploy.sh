b4e11f86d5041a755ffb6dfc4a457bd2bc2e6612
Jason Pickering <jason.p.pickering@gmail.com>
2017-01-08 09:42:35 +0100

    Deploy script tweaks.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 THIS_REPO = "dhis2/translations-app"
-BRANCH_REGEX="2.[0-9][0-9]|master"
+BRANCH_REGEX="2.2[0-9]|master"
 
 if [ "$TRAVIS_REPO_SLUG" == $THIS_REPO ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
  && [[ "$TRAVIS_BRANCH" =~ $BRANCH_REGEX ]] && [[ -z $TRAVIS_TAG ]]; then
