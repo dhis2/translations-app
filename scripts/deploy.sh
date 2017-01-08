366d0b476955b28095269d8cbfeb834dbbbfe947
Jason Pickering <jason.p.pickering@gmail.com>
2017-01-08 09:56:25 +0100

    Minor change to deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 THIS_REPO = "dhis2/translations-app"
 BRANCH_REGEX="2.2[0-9]|master"
 
-if [ "$TRAVIS_REPO_SLUG" == $THIS_REPO ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
+if [ "$TRAVIS_REPO_SLUG" == "$THIS_REPO" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
  && [[ "$TRAVIS_BRANCH" =~ $BRANCH_REGEX ]] && [[ -z $TRAVIS_TAG ]]; then
     
     set -e # exit with nonzero exit code if anything fails
