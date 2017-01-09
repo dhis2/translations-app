b5a7a1f246dc24dae23f0e3dcc9d21fafe250388
Jason Pickering <jason.p.pickering@gmail.com>
2017-01-09 06:57:55 +0100

    Deploy script changes to always deploy master, and branches when tagged.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 THIS_REPO="dhis2/translations-app"
-BRANCH_REGEX="2.2[0-9]|master"
+BRANCH_REGEX="v2.[2-3][0-9]"
 
 if [ "$TRAVIS_REPO_SLUG" == "$THIS_REPO" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
- && [[ "$TRAVIS_BRANCH" =~ $BRANCH_REGEX ]] && [[ -z $TRAVIS_TAG ]]; then
+ && ( ( [[ "$TRAVIS_BRANCH" =~ $BRANCH_REGEX ]] && [[ -z $TRAVIS_TAG ]] ) || \
+[ "$TRAVIS_BRANCH" == "master" ] ); then
     
     set -e # exit with nonzero exit code if anything fails
 
