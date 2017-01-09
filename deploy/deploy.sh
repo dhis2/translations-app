dff1425c3eb9b880375fdddd757f68bc44457b66
Jason Pickering <jason.p.pickering@gmail.com>
2017-01-09 07:25:45 +0100

    Deploy script regex. (#24)

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 THIS_REPO="dhis2/translations-app"
-BRANCH_REGEX="v2.[2-3][0-9]"
+BRANCH_REGEX="^v2.[2-3][0-9]$"
 
 if [ "$TRAVIS_REPO_SLUG" == "$THIS_REPO" ]  && [ "$TRAVIS_PULL_REQUEST" == "false" ] \
  && ( ( [[ "$TRAVIS_BRANCH" =~ $BRANCH_REGEX ]] && [[ -z $TRAVIS_TAG ]] ) || \
