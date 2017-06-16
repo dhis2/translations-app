636d3fb57a0c16f760b1a37408863f474c8317e2
Mark Polak <mark@thedutchies.com>
2017-06-17 00:25:33 +0200

    Remove intellij created js files

--- src/actions.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.APP_START = 'APP_START';
-exports.startApp = function () { return ({ type: exports.APP_START }); };
--- src/d2-helpers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var d2_1 = require("d2/lib/d2");
-var rxjs_1 = require("rxjs");
-exports.d2$ = rxjs_1.Observable.fromPromise(d2_1.getInstance());
-exports.api$ = exports.d2$.map(function (d2) { return d2.Api.getApi(); });
-exports.dbLocales$ = exports.api$.mergeMap(function (api) { return rxjs_1.Observable.fromPromise(api.get('locales/db')); });
-exports.currentUserDbLocale$ = exports.d2$.map(function (d2) { return d2.currentUser.userSettings.keyDbLocale; });
-exports.models$ = exports.d2$.map(function (d2) { return d2.models; });
