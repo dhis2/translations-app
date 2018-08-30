d1968a9d7199c32f53e528c8a3ea14b53ac02746
Joao <joao.antunes@present-technologies.com>
2018-08-30 08:18:09 +0100

    Applying some base style

--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,10 @@
 const CONTENT_AREA_PADDING = 20;
 const HEADER_HEIGHT = '4rem';
+const MAX_WIDTH = 1400;
 
 const styles = {
     contentArea: {
+        maxWidth: MAX_WIDTH,
         paddingTop: HEADER_HEIGHT,
         paddingBottom: CONTENT_AREA_PADDING,
         paddingLeft: CONTENT_AREA_PADDING,
