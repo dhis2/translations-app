bfc84134b68256a7c07234864771a68b02c72be2
Médi-Rémi Hashim <mediremi@gmail.com>
2021-03-05 17:26:49 +0000

    chore: add d2.config.js

---
+++ d2.config.js
@@ -0,0 +1,12 @@
+const config = {
+    type: 'app',
+    name: 'translations',
+    title: 'Translations',
+    coreApp: true,
+
+    entryPoints: {
+        app: './src/App',
+    },
+}
+
+module.exports = config
