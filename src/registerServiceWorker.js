cfbd16d1d92efe4ae23fa309282ca30d3f80ae4e
KaiVandivier <49666798+KaiVandivier@users.noreply.github.com>
2021-07-06 15:54:18 +0200

    fix: unregister service worker (#539)

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -116,6 +116,8 @@ export function unregister() {
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.ready.then(registration => {
             registration.unregister()
+        }).catch(error => {
+            console.error(error.message)
         })
     }
 }
