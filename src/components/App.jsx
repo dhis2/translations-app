f5fef7bb46e7401946181cd86dcc676ac23f8c42
Thomas Zemp <tom@dhis2.org>
2026-01-22 15:10:13 +0100

    fix: add dashboardItem [DHIS2-20385] (#947)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { useConfig } from '@dhis2/app-runtime'
 import D2UIApp from '@dhis2/d2-ui-app'
 import PropTypes from 'prop-types'
 import React from 'react'
 import TranslationsPage from '../pages/Translations/index.jsx'
 import styles from '../styles.js'
 
-const App = ({ d2 }) => (
-    <D2UIApp>
-        <div style={styles.contentArea}>
-            <TranslationsPage d2={d2} />
-        </div>
-    </D2UIApp>
-)
+const App = ({ d2 }) => {
+    const { apiVersion } = useConfig()
+    const featureToggles = {
+        excludedObjects:
+            Number.parseInt(apiVersion) >= 43 ? [] : ['dashboardItem'],
+    }
+    return (
+        <D2UIApp>
+            <div style={styles.contentArea}>
+                <TranslationsPage d2={d2} featureToggles={featureToggles} />
+            </div>
+        </D2UIApp>
+    )
+}
 
 App.propTypes = {
     d2: PropTypes.object.isRequired,
