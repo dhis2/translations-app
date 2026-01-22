0d84048da177947611f6e28ce9a4ab8089dac40a
Thomas Zemp <tom@dhis2.org>
2026-01-22 15:01:09 +0100

    fix: clean up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,7 +8,8 @@ import styles from '../styles.js'
 const App = ({ d2 }) => {
     const { apiVersion } = useConfig()
     const featureToggles = {
-        excludedObjects: parseInt(apiVersion) >= 43 ? [] : ['dashboardItem'],
+        excludedObjects:
+            Number.parseInt(apiVersion) >= 43 ? [] : ['dashboardItem'],
     }
     return (
         <D2UIApp>
